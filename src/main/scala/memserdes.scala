2b3722b12e1fc2ef0d46226bcf32bec76fa5fdc3
Albert Magyar <magyar@eecs.berkeley.edu>
2015-09-08 17:34:08 -0700

    Fix mem naming in memserdes to use setMemName

--- src/main/scala/memserdes.scala
+++ src/main/scala/memserdes.scala
@@ -232,7 +232,7 @@ class HellaFlowQueue[T <: Data](val entries: Int)(data: => T) extends Module
   do_flow := empty && io.deq.ready
 
   val ram = SeqMem(data, entries)
-  ram.setName("Flow_Queue_Mem")
+  ram.setMemName("Flow_Queue_Mem")
   when (do_enq) { ram.write(enq_ptr, io.enq.bits) }
 
   val ren = io.deq.ready && (atLeastTwo || !io.deq.valid && !empty)
